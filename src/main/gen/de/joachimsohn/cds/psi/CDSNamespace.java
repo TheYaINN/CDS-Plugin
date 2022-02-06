03f1e86d73a9e08da3eb4466afdb3d72dbe421b9
Bengt Joachimsohn <47212917+TheYaINN@users.noreply.github.com>
2022-02-06 16:05:15 +0100

    Delete src/main/gen directory (#9)
    * [0.3.4] Improves Syntax, added annotations, fixed highlighting, strings, comments, literals & pipeline
    
    * 0.3.2
    
    - fixes supported IntelliJ version
    
    * [v0.3.3] Updated Readme, added Constants, file structure changed
    
    * [v0.3.3] update
    
    - fixes Literal Strings in CDS
    - README refactoring
    
    * [v0.3.3] release
    
    new:
    - CDSCompletionContributor (always only has 3 contributions - It's stupid, but it's a start)
    
    fixes:
    - Literal Strings in CDS
    - Datatypes
    
    refactoring:
    - README
    - Plugin
    
    * [v0.3.3] gitignore
    
    - fixed gitignore, to ignore generated files
    
    * [v0.3.4]
    
    - fixes gitignore
    
    * [v0.3.4]
    
    - fixes gitignore
    
    * [v0.3.4]
    
    - fixes gitignore
    - fixes bnf for strings
    
    * [v0.3.4]
    
    - fixes not null
    - fixes boolean highlighting
    
    * [v0.3.4]
    
    - added codestyle basic implementation
    
    * [v0.3.4]
    
    - refactoring
    
    * [v0.3.4]
    
    - fixed comments highlighting
    
    * [v0.3.4]
    
    - updated BNF
    
    * [v0.3.4]
    
    - updated updated highlighting
    
    * [v0.3.4]
    
    - fixes build and pipeline
    
    * [v0.3.4]
    
    - removes pipeline
    
    * Delete src/main/gen directory

--- src/main/gen/de/joachimsohn/cds/psi/CDSNamespace.java
+++
@@ -1,8 +0,0 @@
-// This is a generated file. Not intended for manual editing.
-package de.joachimsohn.cds.psi;
-
-import com.intellij.psi.PsiElement;
-
-public interface CDSNamespace extends PsiElement {
-
-}
